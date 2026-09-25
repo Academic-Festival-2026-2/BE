d9d2e43cb32642ea1cfc21d32ae74113e00cb8fd
White-ale <godaesyn001@gmail.com>
2026-09-26 00:51:55 +0900

    [feat] 공통 예외 처리 및 회원가입 에러 코드 추가 #8

--- BINTEUM/src/main/java/com/binteum/global/code/ErrorCode.java
+++ BINTEUM/src/main/java/com/binteum/global/code/ErrorCode.java
@@ -3,7 +3,12 @@ package com.binteum.global.code;
 import org.springframework.http.HttpStatus;
 
 public enum ErrorCode {
-  INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 내부 오류가 발생했습니다.");
+  INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 내부 오류가 발생했습니다."),
+  INVALID_INPUT(HttpStatus.BAD_REQUEST, "COMMON400", "입력값이 올바르지 않습니다."),
+
+  // User
+  DUPLICATE_EMAIL(HttpStatus.CONFLICT, "USER409_1", "이미 가입된 이메일입니다."),
+  DUPLICATE_STUDENT_ID(HttpStatus.CONFLICT, "USER409_2", "이미 가입된 학번입니다.");
 
   private final HttpStatus httpStatus;
   private final String code;
